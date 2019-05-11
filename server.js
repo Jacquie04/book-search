1fd9e7dc4fd62186649771da0bf501727bd6d514
Jacquelyn Duley <45111833+Jacquie04@users.noreply.github.com>
2019-05-11 12:26:25 -0700

    googlebooks

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes") 
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,14 +12,16 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// Define API routes here
-
+// Define API routes 
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
